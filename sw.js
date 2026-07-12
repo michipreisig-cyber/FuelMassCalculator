b9835472175421c0c90272d03018a87fbbcc6d9f
michipreisig-cyber <michi.preisig@gmail.com>
2026-07-12 14:38:24 +0200

    Add service worker for offline asset caching
    Implement service worker for offline caching of assets.

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
+const CACHE_NAME = 'offline-app-v1';
+// Add all assets you need offline (HTML, CSS, JS, Images)
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.png' 
+];
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
